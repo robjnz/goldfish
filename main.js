9c9c857391931005a71cc971e83ed95c25b4c3f9
robjnz <robjnz@outlook.com>
2016-11-02 17:32:55 +0000

    add new slide in menu

--- main.js
+++ main.js
@@ -6,9 +6,18 @@ document.getElementById("myBtn").addEventListener("click", displayDate);
 function displayDate() {
     document.getElementById("Date").innerHTML = Date();
   
-    }  
+    } 
 
-    
+window 
+  
+}(this));
 
-    
-    }(this));
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
