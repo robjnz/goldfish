fcb8006b8e5153080c78885c65ac1057fddbee8c
robjnz <robjnz@outlook.com>
2016-11-02 16:36:11 +0000

    add event listner date

---
+++ main.js
@@ -0,0 +1,14 @@
+ (function(window){
+     
+    
+document.getElementById("myBtn").addEventListener("click", displayDate);
+
+function displayDate() {
+    document.getElementById("Date").innerHTML = Date();
+  
+    }  
+
+    
+
+    
+    }(this));
